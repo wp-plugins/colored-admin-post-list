277428184f090b02f9d3533df8ac59a54e8bc0ad
rockschtar <rockschtar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 16:57:17 +0000

    Version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/colored-admin-post-list/trunk@979851 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.4/tags/1.3/scripts/settings.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+    jQuery(".capl-wp-color-picker").wpColorPicker();
+
+
+    $("#capl-form-reset-to-defaults").bind("submit", function() {
+
+        var confirm_message = $("#capl-button-reset-to-defaults").data("message");
+
+        if(confirm(confirm_message)) {
+            return true;
+        }
+
+        return false;
+    });
+
+});
